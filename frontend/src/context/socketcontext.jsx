eefec2110d4b16927e00e698e530c19eb4014fa1
Nishant0121 <142099904+Nishant0121@users.noreply.github.com>
2024-11-25 20:52:45 +0530

    fix with cred in front end

--- frontend/src/context/socketcontext.jsx
+++ frontend/src/context/socketcontext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chit-chat-7gt1.onrender.com", {
+      const socket = io("https://n-chat-av13.onrender.com", {
         query: {
           userId: authUser._id,
         },
